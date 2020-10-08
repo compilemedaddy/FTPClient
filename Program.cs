4a4e96a3e6ba6cac4568f5cb117e1ceedf8e01f0
compilemedaddy <compilemedaddy@users.noreply.github.com>
2020-10-08 13:40:03 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+namespace FTPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://www.contoso.com/test.htm");
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+            request.Credentials = new NetworkCredential("anonymous", "janeDoe@contoso.com");
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            Console.WriteLine(reader.ReadToEnd());
+
+            Console.WriteLine($"Download Complete! status {response.StatusDescription}");
+
+            reader.Close();
+            response.Close();
+
+        }
+    }
+}
